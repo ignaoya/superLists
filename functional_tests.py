4088d548b63b76a801389190b91d95f89669f72e
Ignacio Oyarzabal <ignaoya93@gmail.com>
2019-03-02 15:44:41 +0100

    FT refactor

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Lucas has heard about a cool new online to-do app. He goes
         # to check out its homepage
@@ -33,10 +38,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Write novel premise" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Write novel premise', [row.text for row in rows])
+        self.check_for_row_in_list_table('1:Write novel premise')
 
         # There is still a text box inviting him to add another item. He 
         # enters "Write novel plotpoints" (Lucas is very methodical)
@@ -46,10 +48,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on his list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Write novel premise', [row.text for row in rows])
-        self.assertIn('2:Write novel plotpoints', [row.text for row in rows])
+        self.check_for_row_in_list_table('1:Write novel premise')
+        self.check_for_row_in_list_table('2:Write novel plotpoints')
 
         # Lucas wonders whether the site will remember his list. Then he sees
         # that the site has generated a unique URL for him -- there is some
